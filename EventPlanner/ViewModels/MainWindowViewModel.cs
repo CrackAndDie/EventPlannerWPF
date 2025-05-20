637aa68800dd66636f4f534efbe523fef351ea9d
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2025-05-20 17:11:23 +0300

    Avalonia project created

---
+++ EventPlanner/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EventPlanner.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ EventPlanner/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace EventPlanner.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
